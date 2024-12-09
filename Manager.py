39e3336c741058eb8094a5144b04ec7891c46bd7
Greenloop36 <95960583+Greenloop36@users.noreply.github.com>
2024-12-09 20:33:40 +0000

    Work

--- Manager.py
+++ Manager.py
@@ -6,6 +6,7 @@ import colorama
 from colorama import Fore, Back, Style
 import os
 import subprocess
+import sys
 
 # Init
 colorama.init(autoreset=True)
@@ -16,7 +17,7 @@ Login: str = os.getlogin()
 
 # Functions
 def AdvanceCd(Path: str):
-    CurrentDirectory += f"/{Path}"
+    return CurrentDirectory + f"/{Path}"
 
 def RewindCd():
     CurrentDirectory = os.path.dirname(CurrentDirectory)
@@ -31,5 +32,60 @@ def GetCommandArgs(Args: list, Separator: str | None, SeparatorEnd: int | None):
         CommandArgs: str = Args[1]
         Parsed: list[str] = CommandArgs.split(Separator, maxsplit=SeparatorEnd)
 
+def FileInCd(Name: str):
+    return CurrentDirectory + "/" + Name
+
+def Error(Message: str):
+    print(f"{Fore.RED}error{Fore.RESET}: {Message}")
+
+def Warning(Message: str):
+    print(f"{Fore.LIGHTYELLOW_EX}warning{Fore.RESET}: {Message}")
+
+def Notice(Message: str):
+    print(f"{Fore.LIGHTMAGENTA_EX}notice{Fore.RESET}: {Message}")
+
+def Success(Message: str):
+    print(f"{Fore.GREEN}success{Fore.RESET}: {Message}")
+
+def CustomException(Message: str):
+    print(f"{Fore.LIGHTRED_EX}{Message}{Fore.RESET}")
+
+def Quit(Message: str, ErrorCode: int = 0):
+    CustomException(Message)
+    sys.exit(ErrorCode)
+
 class Commands_Container:
-    pass
+    def cd(*args):
+        path = GetCommandArgs(args)
+
+        if os.path.exists(path):
+            CurrentDirectory = path
+        elif os.path.exists(AdvanceCd(path)):
+            CurrentDirectory = AdvanceCd(path)
+        elif path == "":
+            RewindCd()
+        else:
+            Error(f"Unknown directory \"{Fore.BLUE}{path}{Fore.RESET}\"!")
+    
+
+    def lst(*args):
+        for i in os.listdir(CurrentDirectory):
+            if os.path.isfile(i):
+                print(f"{i}")
+            else:
+                print(f"{Fore.YELLOW}{i}/{Fore.RESET}")
+    
+    def launch(*args):
+        os.startfile(FileInCd())
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            Query: str = input(GetInputPrefix())
+        except KeyboardInterrupt:
+            Quit("KeyboardInterrupt")
+        except EOFError:
+            continue
+        
+        
